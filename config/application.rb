f2a2e66f074db16ff1f9e6ee3c2a59ca3e7c01ba
Unknown <loren.nicolas@gmail.com>
2018-08-12 13:09:16 +0800

    require active_storage/engine

--- config/application.rb
+++ config/application.rb
@@ -3,12 +3,15 @@ require_relative 'boot'
 require "rails"
 # require 'rails/all'
 require "action_controller/railtie"
+require "active_model/railtie"
 require "active_job/railtie"
+# require "active_record/railtie"
 require "action_view/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 require "action_cable/engine"
+require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
